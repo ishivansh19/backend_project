a463e29d334a1877deb0f422882cdfdac888e5e5
Shivansh Mishra <shivanshmishra151@gmail.com>
2024-09-19 23:34:23 +0530

    async handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,31 @@
+import { Promise } from "mongoose"
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch(next);
+    };
+};
+// Promise.resolve() ensures that the result of requestHandler is handled as a promise.
+// If the promise resolves, it continues as normal.
+// If it rejects (i.e., an error occurs), .catch(next) catches the error and passes it to the Express error-handling middleware using next(err).
+// This simplifies error handling for asynchronous operations.
+export {asyncHandler}
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
